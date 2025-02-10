04e3b7a29218097756868c99d08bf2d29592ff7f
Bryson <bryson@streamliners.dev>
2025-02-09 17:49:08 -0800

    Bump minimum Python version to 3.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Bryson Thill",
     packages=["spanish_flashcard_builder"],
     include_package_data=True,
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     install_requires=[
         "requests>=2.26.0",
         "python-dotenv>=0.19.0",
